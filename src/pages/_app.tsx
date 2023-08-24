b07c2826d49253e95950bfd30fbbef4ee3c6f20f
Johans Neira <77376519+johansneirap@users.noreply.github.com>
2023-08-23 22:38:54 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider } from 'next-themes'
 import { SessionProvider } from 'next-auth/react'
-import '@/styles/globals.css'
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
